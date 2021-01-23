b74306b95ea80ba6daca74bf8cd0239d56ec62d2
Justisegod <71340661+Justisegod@users.noreply.github.com>
2021-01-23 15:54:43 +0200

    Delete Burger directory

--- Burger/js/script.js
+++
@@ -1,6 +0,0 @@
- $(document).ready(function() {
-   $('.header-burger').click(function() {
-      $('.header-burger,.header-menu').toggleClass('active');
-      $('body').toggleClass('lock');
-   });
- });
