2d91450cea0b5f6d3f86c6a8731ba9000f141c67
Justisegod <lordrackon@gmail.com>
2021-07-29 18:53:10 +0300

    upd

--- Grid/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.header__burger').click(function() {
-       $('.header__burger,.header__sidebar').toggleClass('active');
-       $('body').toggleClass('lock');
-    });
-  });
