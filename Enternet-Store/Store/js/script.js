9611c04783b5622217f775fbfa481b17e21301d6
Justisegod <71340661+Justisegod@users.noreply.github.com>
2021-08-02 12:36:32 +0300

    Add files via upload

---
+++ Enternet-Store/Store/js/script.js
@@ -0,0 +1,27 @@
+// const cars = ['mazda',' Mers','BMW']
+
+// console.log(cars.length)
+// console.log(cars[0])
+// console.log(cars[1])
+// console.log(cars[2])
+
+// cars[2] = 'Porshe'
+
+// console.log(cars[2])
+
+// cars[cars.lenght] = 'PIZSD'
+
+// console.log(cars)
+
+
+const cars = ['Mazda', 'Mers' , 'Ford']
+
+// for (let i = 0; i < cars.length; i++)
+// {
+// 	const car = cars[i]
+// 	console.log(car)
+// }
+
+for (let car of cars){
+	console.log(car)
+}
