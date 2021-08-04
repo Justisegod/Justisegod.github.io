9eca0bd1ef946a10bf7e23c0889ddf2641c72b91
Justisegod <71340661+Justisegod@users.noreply.github.com>
2021-08-04 15:37:18 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,13 @@ $(function () {
     $('.rightside-menu').addClass('rightside-menu--close');
   })
   
+
+  $('.header__btn-menu').on('click', function () {
+    $('.menu').toggleClass('menu--open');
+});
+
+
+
   $('.top__slider').slick({
     dots: true,
     arrows: false,
