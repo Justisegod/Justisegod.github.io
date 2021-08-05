07e28cbdaad02e88c0c7bb88236be9244889d1d8
Justisegod <71340661+Justisegod@users.noreply.github.com>
2021-08-05 18:03:46 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -32,19 +32,81 @@ if($(window).width() < 651){
     dots: true,
     arrows: false,
     slidesToScroll: 10,
-    slidesToShow: 10
+    slidesToShow: 10,
+    responsive: [ 
+      {
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 8,
+          
+
+        }
+      },
+      {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 7,
+          slidesToScroll: 7,
+          
+
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+          
+
+        }
+      },
+      {
+        breakpoint: 770,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          
+
+        }
+      },
+      {
+        breakpoint: 660,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          
+
+        }
+      },
+      {
+        breakpoint: 545,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          
+
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          
+
+        }
+      },
+      
+
+    ]
   });
   $('.article-slider__box').slick({
     nextArrow:
     '<button  type="button" class="article-slider__arrow article-slider__arrowright "><img src="images/arrow-slider-right.svg" alt="arrow right"></button>',
-    
-
 
     prevArrow:
     '<button  type="button" class="article-slider__arrow  article-slider__arrowleft"><img src="images/arrow-slider-left.svg" alt="arrow left"></button>'
-
-
-    
     
   });
 
