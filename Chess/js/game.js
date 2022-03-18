c855716d20944417e3a2c66ddd2b0ca458f2c969
Justisegod <71340661+Justisegod@users.noreply.github.com>
2022-03-18 10:45:46 +0200

    Add files via upload

--- Chess/js/game.js
+++ Chess/js/game.js
@@ -1,10 +1,18 @@
+//Импортируем Chess API
+// let ChessWebAPI = require('chess-web-api');
+
+// let chessAPI = new ChessWebAPI();
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
     function drawBoard(){
         const header = document.querySelector('.header');
         const footer = document.querySelector('.footer');
         const board = document.querySelector('.board');
         const game = document.querySelector('.game');
-        const gameHeight = game.clientHeight - header.clientHeight - footer.clientHeight;
+        // const gameHeight = game.clientHeight - header.clientHeight - footer.clientHeight;
         const gameContainer = document.querySelector('.game__container');
         let gameContainerWSize = gameContainer.clientWidth;
     
@@ -18,7 +26,6 @@ document.addEventListener('DOMContentLoaded', function () {
     
     
     
-        console.log(gameHeight);
     }
     drawBoard();
     
@@ -35,12 +42,23 @@ document.addEventListener('DOMContentLoaded', function () {
       let allSeconds = minutes * 60 + seconds;
       //делим круг таймера на 4 части
       let oneHalfOfRound = allSeconds / 4;
+      //половинки счётчика 
+      let round = [
+        document.querySelector('.timer-body__item--two'),
+        document.querySelector('.timer-body__item--four'),
+        document.querySelector('.timer-body__item--three'),
+        document.querySelector('.timer-body__item--one'),
+      ]
+      //index for round 
+      let roundIndex = 0;
 
       function countdownTimerr() {
         seconds--;
-    
+        allSeconds--;
+
         if(minutes === 0 & seconds <= 0) {
           clearInterval(timerId);
+          paintAllPeacesBack();
           console.log('the Time is over');
         }
         if(seconds <= 0 & minutes > 0) {
@@ -50,17 +68,28 @@ document.addEventListener('DOMContentLoaded', function () {
         $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
         $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
 
-        drawRound();
+        //Обновляем части счётчика, закрашивая в белый
+        if(allSeconds % Math.round(oneHalfOfRound) == 0 & allSeconds > 0) {
+          paintPeace(roundIndex++,'#fff');
+        }
       }
-      function drawRound() {
-          allSeconds--;
-          console.log(oneHalfOfRound);
+      function paintPeace(index ,hex = '#fff') {
+        if(index >= 4) {
+          index = 0;
+        }
 
+        round[index].style.background = `${hex}`;
+        console.log(`Painted by ${hex}`, index, allSeconds, oneHalfOfRound, Math.round(oneHalfOfRound));
+      }
+      function paintAllPeacesBack() {// закрашивает все части после конца таймера
+        for(let i = 0; i < 4; i++) {
+          paintPeace(i, 'transparent');
+        }
       }
       
       timerId = setInterval(countdownTimerr, 1000);
     }
-    countIt(10, 1);
+    countIt(10, 0);
   });
   
 
